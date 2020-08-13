052493591cce342ff8fa1da99b5e1c3bdcdaf85f
mohsen <mo.jalali90@gmail.com>
2020-08-13 16:45:22 +0430

    delete extra file

--- gatsby-theme-code-notes/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-const React = require('react')
-const { SearchProvider } = require('./src/components/Search')
-
-exports.wrapRootElement = ({ element }) => {
-  return <SearchProvider>{element}</SearchProvider>
-}
--- gatsby-theme-code-notes/src/gatsby-plugin-theme-ui/sizes.js
+++
@@ -1,6 +0,0 @@
-export const sizes = {
-  container: 900,
-  sidebar: '240px',
-  sidebarSkinny: '50%',
-  contentMaxWidth: '900px',
-}
